509570e18013a5a1a440743b896a3aed8d491a34
Unknown <alex.r246@stu.nebo.edu>
2018-01-23 13:09:02 -0700

    created game start

--- adventureGame/script.js
+++ adventureGame/script.js
@@ -1,28 +1,39 @@
-
-/* alert("ROBOT: 'Boop!'");
-        confirm("The robot seems friendly");
-        prompt("Are you going to befriend it?");*/
-
-var favColor = prompt("What is your favorite color?").toLowerCase();
-        
-        if(favColor == "blue") {
-            alert("The sky is blue");
-        }
-        else if(favColor == "red") {
-            alert("Fire is red");
-        }
-        else if(favColor == "green") {
-            alert("Grass is green");
-        }
-        else if(favColor == "yellow") {
-            alert("The sun is yellow");
-        }
-        else if(favColor == "white") {
-            alert("Walls are white");
-        }
-        else if(favColor == "black") {
-            alert("Black is wrong");
+start();
+function start() {
+    
+    var playerName = prompt('What is your name traveler?');
+    
+    alert('Welcome ' + playerName + " to the land of Zer'Rek");
+    
+    var startGame = confirm('Are you ready to begin your adventure?');
+    
+        if(startGame){
+            village();
         }
         else {
-            alert("No bad human");
+            alert('goodbye');
         }
+}
+
+
+function village() {
+    var village = prompt('You arrive at a small village. There is a blacksmith, inn, shop, and stable. Where will you go?').toLowerCase();
+    
+    switch(village) {
+        case 'blacksmith':
+            alert('What can I get for you young lad?');
+        break;
+        case 'inn':
+            alert('A gorgeous young lady innkeeper ask "Are you wery, will you rest?"');
+        break;
+        case 'shop':
+            alert('If you get gold i got stock what ye looking for?');
+        break;
+        case 'stable':
+            alert('Will you use a horse to travel to another city?');
+        break;
+        default:
+            alert('I dont understand ' + village);
+        break;
+    }
+}
