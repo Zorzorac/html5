d8de7be2bd1fd76295f827e80b6c1a197e8b2c47
Unknown <alex.r246@stu.nebo.edu>
2018-03-27 12:16:27 -0600

    finished

--- colorPicker/script.js
+++ colorPicker/script.js
@@ -1,39 +1,42 @@
 var modal = document.getElementById("modal");
+
 var changeBtn = document.getElementById("change");
+
 var preview = document.getElementById("preview");
 
-
 function Preview() {
-    color = document.querySelector('input[name="color"]:checked').value;
-    
-    console.log("the color is " +color);
-    
-    preview.style.background = color;
+
+	color = document.querySelector('input[name="colors"]:checked').value;
+
+	console.log("The color is " + color);
+
+	preview.style.background = color;
 }
 
 function changeColor() {
-    console.log("the color is " + color);
-    
-    document.body.style.background = color;
+
+	console.log("The color is: " + color);
+
+  document.body.style.background = color;
 }
 
 changeBtn.onclick = function() {
-    modal.style.visibility = "visible";
-    modal.style.opacity = "1";
+	modal.style.visibility = "visible";
+  modal.style.opacity = "1";
 }
 
 function Close() {
+	modal.style.visibility = "hidden";
+  modal.style.opacity = "0";
+	preview.style.background = "#1a1a1a";
+	document.querySelector('input[name="colors"]:checked').checked = false;
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.visibility = "hidden";
     modal.style.opacity = "0";
     preview.style.background = "#1a1a1a";
     document.querySelector('input[name="colors"]:checked').checked = false;
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.visibility = "hidden";
-        preview.style.opacity = "0";
-        preview.style.background = "#1a1a1a";
-        document.querySelector('input[name="colors"]:checked').checked = false;
-    }
+  }
 }
